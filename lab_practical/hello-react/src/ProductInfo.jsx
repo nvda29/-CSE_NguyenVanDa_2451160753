f975eaf5ba4bdcbacd41398c196a1f809a3de65b
Nguyen Van Da <nvda292006@gmail.com>
2026-05-31 13:38:01 +0700

    [COMPONENT] Create ProductInfo component

---
+++ lab_practical/hello-react/src/ProductInfo.jsx
@@ -0,0 +1,16 @@
+function ProductInfo() {
+  return (
+    <div className="product">
+      <h2>iPhone 15</h2>
+      <p className="price">25.000.000 d</p>
+      <ul>
+        <li>Man hinh: 6.1 inch</li>
+        <li>Camera: 48MP</li>
+        <li>Pin: 3349 mAh</li>
+      </ul>
+      <button type="button">Mua ngay</button>
+    </div>
+  )
+}
+
+export default ProductInfo
